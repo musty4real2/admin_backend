1081718b69fd8937186e0482cb8eded5e26335c7
musty4real2 <musty4real2@gmail.com>
2024-09-05 21:33:22 +0100

    pagination fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import restProvider from 'ra-data-simple-rest';
 import PostList from './components/PostList';
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { List, Datagrid, TextField, DateField, EditButton, DeleteButton } from 'react-admin';
 
 const PostList = (props) => {
